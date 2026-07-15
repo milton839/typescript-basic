eb610e9f23d178d3e6d4af5a9bd90b1ed39f7c46
milton839 <miltonbiswas839@gmail.com>
2026-07-15 20:22:53 +0600

    Array destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -11,3 +11,13 @@ const student = {
 const { profession: myProfession, name: { lastname:mylastname } } = student;
 const { lastname } = student.name; // 2 vabei kora jai
 console.log(myProfession, lastname)
+
+//array destructuring
+const friends = ["Jhankar", "Rakib", "Sakib", "Rasel", "Shuvo"];
+// const [firstFriend, secondFriend, ...restFriends] = friends;
+
+const [, secondFriend] = friends; // 2nd friend er jonno comma use kora hoyeche
+console.log(secondFriend);
+
+const [, , , fourthFriend] = friends; // 4th friend er jonno 3 comma use kora hoyeche
+console.log(fourthFriend);
